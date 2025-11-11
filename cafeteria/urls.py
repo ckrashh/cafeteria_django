c10cd7a82fcd25662dad538972053d2ef1e5ee20
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-11-10 22:47:31 -0300

    arreglo

--- cafeteria/urls.py
+++ cafeteria/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('miapp.urls')),
     path('sesion/', include('sesion.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
