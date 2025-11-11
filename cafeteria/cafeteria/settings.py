9f896217ffe955ae05a22e3e7674c9287348c785
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-11-10 21:56:16 -0300

    aaaaaaa

--- cafeteria/cafeteria/settings.py
+++ cafeteria/cafeteria/settings.py
@@ -27,8 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'miapp',
-    'sesion',
+    'cafeteria.miapp',
+    'cafeteria.sesion',
 ]
 
 MIDDLEWARE = [
